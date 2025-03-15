94c84a57cb3e2e7b86d840f4e56a70f56e6a8985
Raheem Wilson <wilsonzackery16@gmail.com>
2025-03-15 14:22:52 -0500

    refactor: move home page to marketing module and update routing

--- src/modules/auth/routes/index.tsx
+++ src/modules/auth/routes/index.tsx
@@ -1,13 +1,8 @@
 import { RouteObject } from "react-router-dom";
 import { LoginPage } from "../pages/login";
 import RegisterPage from "../pages/register";
-import Home from "../pages/home";
 
 export const authRoutes: RouteObject[] = [
-  {
-    path: "/",
-    element: <Home />,
-  },
   {
     path: "login",
     element: <LoginPage />,
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,10 @@
 import { authRoutes } from "@/modules/auth/routes";
 import { locationRoutes } from "@/modules/locations/routes";
+import { marketingRoutes } from "@/modules/marketing/routes";
 import { createBrowserRouter } from "react-router-dom";
 
 export const appRouter = createBrowserRouter([
   ...authRoutes,
   ...locationRoutes,
+  ...marketingRoutes
 ]);
