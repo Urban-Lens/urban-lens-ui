a3d17f867d0a31c428f162655c86c5c983eef0db
Jason Gayle <jasonblue1626@gmail.com>
2025-03-15 12:51:08 -0400

    feat: added omitted files

---
+++ src/modules/auth/hooks/login.ts
@@ -0,0 +1,33 @@
+import { useMutation } from "@tanstack/react-query";
+import { axios } from "@/lib/axios";
+
+import { toast } from "sonner";
+import { ILoginCredentials, ILoginResponse } from "../types";
+
+const login = async (
+  credentials: ILoginCredentials
+): Promise<ILoginResponse> => {
+  const { data } = await axios.post<ILoginResponse>("/auth/login", credentials);
+  return data;
+};
+
+export const useLogin = (
+  onSuccess: (loginResponse: ILoginResponse) => void
+) => {
+  return useMutation<ILoginResponse, Error, ILoginCredentials>({
+    mutationFn: async (loginCredentials: ILoginCredentials) => {
+      const response = await login(loginCredentials);
+      return response;
+    },
+    onError: (_error: Error) => {
+      toast.error("Invalid username or password. Please try again.");
+    },
+
+    onSettled: async (loginResponse: ILoginResponse | undefined) => {
+      if (loginResponse) {
+        toast.success("Login Successful");
+        onSuccess(loginResponse);
+      }
+    },
+  });
+};
